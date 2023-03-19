e2dee729b298e498902d580c1518e9c25486fdf1
madhukmd <kondamadhu1994@gmail.com>
2023-03-19 16:42:44 +0000

    Navigation Blog

---
+++ src/components/Header/index.js
@@ -0,0 +1,29 @@
+import {Link} from 'react-router-dom'
+import './index.css'
+
+const Header = () => (
+  <nav className="nav-header">
+    <div className="blog-container">
+      <h1 className="blog-title">My Blog</h1>
+      <ul className="nav-menu">
+        <li>
+          <Link to="/" className="link-color">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/about" className="link-color">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="/contact" className="link-color">
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+)
+
+export default Header
